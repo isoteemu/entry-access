dc9aa5da981e376930a8714d8d711eefa8d8adfd
Teemu Autto <teemu.a.autto@jyu.fi>
2025-12-03 14:05:38 +0200

    feat: integrate NoSleep.js for preventing device sleep and update script tags in templates

--- web/assets/js/device.js
+++ web/assets/js/device.js
@@ -1,4 +1,7 @@
-document.addEventListener('DOMContentLoaded', async () => {
+
+// Prevent device from sleeping using NoSleep.js
+// Notice: NoSleep.js requires a user interaction to activate
+function initNoSleep() {
     const noSleep = new window.NoSleep();
     
     // Need a user interaction to enable NoSleep
@@ -8,4 +11,20 @@ document.addEventListener('DOMContentLoaded', async () => {
         document.removeEventListener('click', enableNoSleep, false);
     }
     document.addEventListener('click', enableNoSleep, false);
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    // Check if NoSleep.js is loaded
+    if (typeof window.NoSleep === 'undefined') {
+        // Load NoSleep.js dynamically
+        const script = document.createElement('script');
+        script.src = 'dist/vendor/js/nosleep.min.js';
+        script.onload = initNoSleep;
+        document.head.appendChild(script);
+    } else {
+        initNoSleep();
+        return;
+    }
 });
+
+
